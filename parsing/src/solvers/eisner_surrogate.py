c3f9f08c6ea1655e89b41fdfd1b65895e28fdf13
berliner <alon.berliner@gmail.com>
2022-05-03 21:40:16 +0300

    Updated an import path

--- parsing/src/solvers/eisner_surrogate.py
+++ parsing/src/solvers/eisner_surrogate.py
@@ -3,7 +3,7 @@ import numpy as np
 from torch.autograd import Function, gradcheck
 from scipy.special import softmax
 
-from decoder_eisner import parse_proj
+from solvers.decoder_eisner import parse_proj
 
 
 def eisner_surrogate(arc_scores, hard=False):
